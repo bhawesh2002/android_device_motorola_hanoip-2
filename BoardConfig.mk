a0e15250006839dc71512d03406f55fbf95f98d8
Nikhil <renjian503@gmail.com>
2022-03-14 11:55:37 +0530

    update # Kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,22 +38,19 @@ TARGET_BOOTLOADER_BOARD_NAME := hanoip
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
+BOARD_BOOT_HEADER_VERSION := 3
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc \
-    LLVM=1
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CONFIG := vendor/hanoip_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/sm6150
-TARGET_KERNEL_VERSION := 4.19
+TARGET_KERNEL_CONFIG := vendor/hanoip_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
-#KERNEL_TOOLCHAIN := $(PWD)/prebuilts/gcc/linux-x86/aarch64/arm64-gcc/bin
-#TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-elf-
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Kernel modules - Audio
 TARGET_MODULE_ALIASES += \
