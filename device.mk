b210b234c30ecdd685a59c979238a7477d0fcb06
Nikhil <renjian503@gmail.com>
2022-03-19 18:30:28 +0530

    build from source

--- device.mk
+++ device.mk
@@ -400,7 +400,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@2.2 \
+    vendor.qti.hardware.perf@2.2.vendor
 
 # QCOM
 PRODUCT_COPY_FILES += \
