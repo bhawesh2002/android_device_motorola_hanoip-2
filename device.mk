1d65f189bbe956da8085a82011f3dd7c135afddd
Nikhil <renjian503@gmail.com>
2022-03-20 10:59:56 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -184,7 +184,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
-    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides_qti.xml
 
 # Camera
 PRODUCT_PACKAGES += \
